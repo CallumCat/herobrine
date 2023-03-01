18dbc9bd241992e3746f1aab764699aa6004450d
Callum <40074095+CallumCat@users.noreply.github.com>
2023-03-01 17:38:21 -0500

    Update about.js

--- commands/Info/about.js
+++ commands/Info/about.js
@@ -5,13 +5,8 @@ module.exports = {
 		return {embed: {
 			title: '**About**',
 			fields:[
-				{name: "Prefixes", value: "`hh!command`, `heyhero command`, or `heyherobrine command`"},
-				{name: "Creator", value: "greysdawn (GreySkies#9950)"},
-				{name: "Repo", value: "https://github.com/greys-bots/herobrine"},
-				{name: "Website", value: "https://hb.greysdawn.com/"},
-				{name: "Support Discord", value: "https://discord.gg/EvDmXGt"},
-				{name: "Creator's Patreon", value: "https://patreon.com/greysdawn"},
-				{name: "Creator's Ko-Fi", value: "https://ko-fi.com/greysdawn"},
+				{name: "Prefixes", value: "`fcm!command`"},
+				{name: "Creator", value: "Callum (Callum#0003)"},
 				{name: "Guilds", value: bot.guilds.size},
 				{name: "Users", value: bot.users.size}
 			]
